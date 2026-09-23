3528158f8b18565f611eafac8bcca5e05f34527d
Dhruv <dhruvchandraprajapati91@gmail.com>
2026-09-23 22:55:07 +0530

    added githubdb

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const express = require('express');
 const app = express();
 const PORT= process.env.PORT || 3000;
 
+const githubdb ={
+  "message": "Not Found",
+  "documentation_url": "https://docs.github.com/rest",
+  "status": "404"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+app.get('/github', (req, res) => {
+  res.json(githubdb);
 });
 
 app.listen(PORT, () => {
